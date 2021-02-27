3256b56382c1c6158eceac251cb48d156b24da51
Sergey Margelov <smargelov@gmail.com>
2021-02-27 16:49:25 +0300

    Replace images source files

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,30 +1,35 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import axios from 'axios';
-import _ from 'lodash'
+import _ from 'lodash';
 
 Vue.use(Vuex);
 
 let store = new Vuex.Store({
     state: {
         products: [],
+        catalog: [],
         cart: [],
     },
     mutations: {
         SET_PRODUCTS_TO_STATE: (state, products) => {
             state.products = products;
         },
+        SET_PRODUCTS_TO_CATALOG: (state) => {
+            state.catalog = _.take(state.products, 6);
+        },
         SET_PRODUCT_TO_CART: (state, product) => {
             state.cart.push(product);
         },
     },
     actions: {
-        GET_PRODUCTS_FROM_API({commit}) {
+        GET_PRODUCTS_FROM_API({ commit }) {
             return axios('http://localhost:3000/catalog', {
                 method: 'GET',
             })
                 .then((products) => {
                     commit('SET_PRODUCTS_TO_STATE', products.data);
+                    commit('SET_PRODUCTS_TO_CATALOG');
                     return products;
                 })
                 .catch((error) => {
@@ -32,13 +37,13 @@ let store = new Vuex.Store({
                     return error;
                 });
         },
-        ADD_PRODUCT_TO_CART({commit}, product) {
+        ADD_PRODUCT_TO_CART({ commit }, product) {
             commit('SET_PRODUCT_TO_CART', product);
         },
     },
     getters: {
         PRODUCTS(store) {
-            return store.products;
+            return store.catalog;
         },
         CART(store) {
             return store.cart;
